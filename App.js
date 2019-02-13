c12d6f0afb9ba406af1711662222d441bdd66ea1
Akash K <akash.kulkarni@pratian.com>
2019-02-13 16:20:00 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,82 @@
+
+import React, { PureComponent } from 'react';
+import { View, Text, NetInfo, Dimensions, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+function MiniOfflineSign() {
+  return (
+    <View style={styles.offlineContainer}>
+      <Text style={styles.offlineText}>No Internet Connection</Text>
+    </View>
+  );
+}
+
+class OfflineNotice extends PureComponent {
+  state = {
+    isConnected: false
+  };
+
+  componentDidMount() {
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = isConnected => {
+    if (isConnected) {
+      this.setState({ isConnected });
+    } else {
+      this.setState({ isConnected });
+    }
+  };
+
+  render() {
+    if (!this.state.isConnected) {
+      return <MiniOfflineSign />;
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>instructions</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  offlineContainer: {
+    backgroundColor: '#b52424',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width,
+    position: 'absolute',
+    top: 30
+  },
+  offlineText: { color: '#fff' }
+});
+
+export default OfflineNotice;
+
+
